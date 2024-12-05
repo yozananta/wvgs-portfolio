f3f625b71fe7d83745c6b239f71c818408ba876e
yozananta <92283172+yozananta@users.noreply.github.com>
2024-12-05 17:37:41 +0700

    Update Honors.js

--- src/components/Honors.js
+++ src/components/Honors.js
@@ -9,6 +9,7 @@ export default function Honors(){
             <p className="font-light text-gray-400">Here are some of my accomplishments.</p>
 
             <div className="flex flex-col md:flex-row mt-4 gap-5">
+                <HonorCard name="Internship at Dinas Pariwisata Kota Batu" issued="Author" desc="Maintenance and troubleshooting of the cultural database on the website https://sininja.site/ and the Cultural Database of Batu City are being conducted to ensure optimal performance and data accuracy." />
                 <HonorCard name="Internship at Axel Bit Training Centre" issued="Author" desc=" I had the opportunity to work on a challenging project involving the designing and implementation of a LAN network. The project encompassed various aspects, including cabling, routing, switching, firewalling, and wireless network configuration." />
                 <HonorCard name="System Information at Gajayana University in Malang" issued="Author" desc="During my second year of studying,I had the opportunity to further develop my skills and knowledge in various aspects of the field. This included gaining practical experience in implementing HTML, Native PHP, Java, Vue.js, React.js, and Mobile Programming." />
                 <HonorCard name="My Academic Journey" issued="author" desc="I have been involved in both internal and external web development projects within the campus environment while pursuing my studies. These projects were undertaken as side ventures alongside my academic commitments. " />
