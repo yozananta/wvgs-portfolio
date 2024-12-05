766dbf2b92a06feb2887b0583e66926d5a1e7d02
yozananta <92283172+yozananta@users.noreply.github.com>
2024-12-05 17:13:10 +0700

    Update Certs.js

--- src/components/Certs.js
+++ src/components/Certs.js
@@ -3,7 +3,8 @@ import CertCard from "./CertCard.js"
 import hr from "../assets/curve-hr.svg"
 import mtcna from "../assets/certs/mtcna.jpg"
 import alterra from "../assets/certs/alterra.jpg"
-import dicodingAws from "../assets/certs/AxelBitrenew.png"
+import axelbit from "../assets/certs/AxelBitrenew.png"
+import dicoding1 from "../assets/certs/sertifikat_course_123_4382228_270924162932.jpg"
 // import dicodingJs from "../assets/certs/dicoding-js.png"
 // import dicodingNetwork from "../assets/certs/dicoding-network.png"
 // import progateHtml from "../assets/certs/progate-htmlcss.png"
@@ -21,9 +22,10 @@ export default function Certs(){
 
             {/* <div className="flex flex-col md:flex-row flex-wrap mt-4 gap-5"> */}
             <div className="grid grid-cols-1 md:grid-cols-3 justify-center mt-4 gap-5">
+                <CertCard name="Dicoding Academy" img={dicoding} issued="Dicoding Academy" date="Sep 2024" />
                 <CertCard name="Bootcamp Alterra Academy" img={alterra} issued="Alterra Academy" date="Mar 2023" />
                 <CertCard name="Mikrotik Certfied Network Associate" img={mtcna} issued="Mikrotik" date="Jul 2019" />
-                <CertCard name="Axel Bit Training Centre" img={dicodingAws} issued="Axel Bit" date="Jan 2020" />
+                <CertCard name="Axel Bit Training Centre" img={axelbit} issued="Axel Bit" date="Jan 2020" />
                 <CertCard name="Coming Soon" img={blankalt} issued="author" date="Mei 2021" />
                 <CertCard name="Coming Soon" img={blankalt} issued="author" date="Jan 2023" />
                 <CertCard name="Coming Soon" img={blankalt} issued="author" date="Dec 21" />
