cbb57e3e5ea86cca4f9b06970f007600d4a2395b
yozananta <92283172+yozananta@users.noreply.github.com>
2026-01-20 14:41:50 +0700

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -9,9 +10,9 @@ import lara from "../assets/skills/lara.png"
 import mikrotik from "../assets/skills/mikrotik.svg"
 import php from "../assets/skills/php.png"
 import java from "../assets/skills/java.png"
-import android from "../assets/skills/android.png"
 import hr from "../assets/curve-hr.svg"
 import vue from "../assets/skills/vue.png"
+import csharp from "../assets/skills/csharp.svg"
 
 export default function Skills() {
     const settings = {
@@ -20,7 +21,24 @@ export default function Skills() {
         infinite: true,
         slidesToShow: 2,
         slidesToScroll: 1
-      };
+    };
+
+    const getExperience = (startYear) => {
+        const currentYear = new Date().getFullYear();
+        const years = currentYear - startYear;
+        return years > 0 ? `${years} Years` : "1 Year";
+    };
+
+    const skillsData = [
+        { name: "C#", img: csharp, startYear: 2022 },
+        { name: "Vue", img: vue, startYear: 2022 },
+        { name: "React", img: reactIcon, startYear: 2022 },
+        { name: "JavaScript", img: javascript, startYear: 2021 },
+        { name: "Mikrotik", img: mikrotik, startYear: 2020 },
+        { name: "PHP", img: php, startYear: 2021 },
+        { name: "Laravel", img: lara, startYear: 2022 },
+        { name: "Java", img: java, startYear: 2021 }
+    ];
 
     return (
         <div id="skills" className="mt-4 text-white">
@@ -29,15 +47,14 @@ export default function Skills() {
 
             <div className="mt-4">
                 <Slider {...settings}>
-                    
-                <SkillCard name="vue" experience="1 years" img={vue} />
-                <SkillCard name="react" experience="1 years" img={reactIcon} />
-                <SkillCard name="javascript" experience="2 years" img={javascript} />
-                <SkillCard name="mikrotik" experience="3 years" img={mikrotik} />
-                <SkillCard name="php" experience="2 years" img={php} />
-                <SkillCard name="laravel" experience="1 years" img={lara} />
-                <SkillCard name="" experience="2 years" img={java} />
-                <SkillCard name="android" experience="2 years" img={android} />
+                    {skillsData.map((skill, index) => (
+                        <SkillCard 
+                            key={index}
+                            name={skill.name}
+                            experience={getExperience(skill.startYear)}
+                            img={skill.img}
+                        />
+                    ))}
                 </Slider>
             </div>
             <img src={hr} className="w-full mt-8 md:h-3" alt="hr" />
