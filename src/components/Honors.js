7d79e3e90021baef9f92eec9a3ba9e8cd721002b
yozananta <92283172+yozananta@users.noreply.github.com>
2026-01-20 15:07:41 +0700

    Update Honors.js

--- src/components/Honors.js
+++ src/components/Honors.js
@@ -27,19 +27,19 @@ export default function Honors(){
                 <HonorCard 
                     name="Backend Developer at Indomaret Group" 
                     issued={`Nov 2025 – Present (${getIndomaretDuration()})`} 
-                    desc="Responsible for developing and maintaining scalable backend services, optimizing database performance, and ensuring seamless API integration to support high-traffic retail operations." 
+                    desc="Developing scalable backend services, optimizing database performance, and ensuring seamless API integration for retail operations." 
                 />
 
                 <HonorCard 
                     name="Internship at Dinas Pariwisata Kota Batu" 
                     issued="Jul 2024 – Sep 2024 (3 months)" 
-                    desc="Maintenance and troubleshooting of the cultural database on the website sininja.site and the Cultural Database of Batu City, ensuring optimal performance and data accuracy." 
+                    desc="Developed cultural web modules, optimized database performance, and created digital dashboards for data visualization." 
                 />
 
                 <HonorCard 
                     name="Internship at Axel Bit Training Centre" 
                     issued="Jul 2019 – Jan 2020 (7 months)" 
-                    desc="Executed a challenging project involving the design and implementation of a LAN network. Scope included cabling, routing, switching, firewalling, and wireless network configuration." 
+                    desc="Assisted IT operations, performed basic troubleshooting, and supported multi-functional teams with technical and administrative tasks." 
                 />
                 
             </div>
